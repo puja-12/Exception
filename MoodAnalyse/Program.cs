ed39d6fd386d939008026a70f4aeeecbbdff87ef
puja <pujarana1796@gmail.com>
2022-05-20 14:58:53 +0530

    puja added Mood Analyse exception

--- MoodAnalyse/Program.cs
+++ MoodAnalyse/Program.cs
@@ -11,19 +11,33 @@ namespace MoodAnalyse
     {
         static void Main(string[] args)
         {
-            HappySadmood mood = new HappySadmood();
-            string result = mood.Analyse("I am in sad Mood");
-            Console.WriteLine(result);
+            while (true)
+            {
+                Console.WriteLine("Please choose the option :\n1)Happy or SAD Mood\n2)Using the Default and Parametterized Constructor\n3)Exception");
+                int option = Convert.ToInt16(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        HappySadmood mood = new HappySadmood();
+                        string result = mood.Analyse("I am in sad Mood");
+                        Console.WriteLine(result);
+                        break;
+                    case 2:
 
-            refractor1 A2 = new();
-            string result2 = A2.Analyse();
-            Console.WriteLine("Using the Default Constructor here and Mood is :" + result2);
-
-
-            refractor1 A3 = new("I am in Happy Mood");
-            string result3 = A3.Analyse();
-            Console.WriteLine("Using the Parameterized Constructor here and Mood is :" + result3);
-
+                        refractor1 A2 = new();
+                        string result2 = A2.Analyse();
+                        Console.WriteLine("Using the Default Constructor here and Mood is :" + result2);                       
+                    
+                        refractor1 A3 = new("I am in Happy Mood");
+                        string result3 = A3.Analyse();
+                        Console.WriteLine("Using the Parameterized Constructor here and Mood is :" + result3);
+                        break;
+                    case 3:
+                        MoodAnalyzerException A4 = new ();
+                        string result4 = A4.ExceptionMethod(null);
+                        break;
+                }
+            }
         }
     }
 }
---
+++ MoodAnalyseTests/MoodAnalyzerExceptionTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyse.Tests
+{
+    [TestClass()]
+    public class MoodAnalyzerExceptionTests
+    {
+        [TestMethod()]
+        public void ExceptionMethodTest()
+        {      
+         
+                MoodAnalyzerException A4 = new();
+                string mood = A4.ExceptionMethod(null);
+                Assert.AreEqual("Happy", mood);
+            }
+    }
+}
